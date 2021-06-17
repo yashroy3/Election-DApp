9aa93d72fccbda35a6d0214ef08d59b00ecffe41
yashroy3 <yashroy252@gmail.com>
2021-06-17 16:55:56 +0530

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Election from "./contracts/Election.json";
+import getWeb3 from "./getWeb3";
+
+
+class App extends Component {
+  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = Election.networks[networkId];
+      const instance = new web3.eth.Contract(
+        Election.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    return (
+      <div>
+        {this.state.accounts[0]}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ contracts/Election.sol
@@ -0,0 +1,70 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+     bool voteprocess =true;
+
+
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
+    uint public actvote=0;
+    uint public regvote=6;
+    uint public votepercent;
+
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
+
+    constructor () public {
+        addCandidate("BJP");
+        addCandidate("Congress");
+        addCandidate("Aap");
+
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+        require(voteprocess);
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+        //voting percentage
+        actvote++;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+    function votepercentage() public{
+
+        votepercent = actvote*100;
+        votepercent = votepercent/regvote;
+
+
+    }
+    //initialize voting process and termination
+    function stopvoting() public{
+        voteprocess=false;
+    }
+}
